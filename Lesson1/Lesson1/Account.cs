4f40a16cfdb13011880d87df1c7cc4fa9f3844ed
Jackson <dmitrievei92@icloud.com>
2022-03-23 19:14:33 +0300

    properties

---
+++ Lesson1/Lesson1/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Account
+    {
+        private int accountNumber;
+        private int balance;
+
+        private enum accountType
+        {
+            Budget,
+            ForeignСurrency,
+            Frozen,
+            Insured,
+            Checking,
+            Correspondent,
+            Savings
+        }
+
+        public int AccountNumber
+        {
+            get
+            {
+                return this.accountNumber;
+            }
+            set
+            {
+                this.accountNumber = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return this.balance;
+            }
+            set
+            {
+                this.balance = value;
+            }
+        }
+
+        public int AccountType
+        {
+            get
+            {
+                return this.AccountType;
+            }
+        }
+
+
+    }
+
+    
+}
