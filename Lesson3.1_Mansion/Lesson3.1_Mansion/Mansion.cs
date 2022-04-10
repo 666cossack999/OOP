a9f34a94584f202880fe74637e33ccb3aef71f06
Jackson <dmitrievei92@gmail.com>
2022-04-10 14:51:10 +0300

    3.1 Final

--- Lesson3.1_Mansion/Lesson3.1_Mansion/Mansion.cs
+++ Lesson3.1_Mansion/Lesson3.1_Mansion/Mansion.cs
@@ -21,7 +21,6 @@ namespace Lesson3._1_Mansion
         public Mansion(int height) : this(height, 0)
         {
         }
-
         public Mansion(int height, int storeys) : this(height, storeys, 0)
         {
         }
@@ -29,7 +28,6 @@ namespace Lesson3._1_Mansion
         public Mansion(int height, int storeys, int apartments) : this(height, storeys, apartments, 0)
         {
         }
-
         public Mansion(int height, int storeys, int apartments, int entrances)
         {
             _mansionNumber = SetId();
@@ -38,16 +36,79 @@ namespace Lesson3._1_Mansion
             _apartments = apartments;
             _entrances = entrances;
         }
+       
+        public void SetHeight(int height) { _height = height; }
 
+        public void SetStoreys (int storeys) { _storeys = storeys; }
 
+        public void SetApartmens (int apartmens) { _apartments = apartmens; }
+        
+        public void SetEntrances (int entrances) { _entrances = entrances; }
+
+        public void PrintId() { Console.WriteLine($"Номер дома: {_mansionNumber}"); }
+
+        public void PrintHeight() { Console.WriteLine($"Высота дома: {_height} см"); }
+
+        public void PrintStoreys() { Console.WriteLine($"Количество этажей: {_storeys}"); }
+
+        public void PrintApartments() { Console.WriteLine($"Количество квартир: {_apartments}"); }
+
+        public void PrintEntrances() { Console.WriteLine($"Количество подъездов: {_entrances}"); }
+
+        private void PrintLine()
+        {
+            Console.WriteLine("\n=======================================\n");
+        }
+
+        /// <summary>
+        /// Вычисляет высоту этажа
+        /// </summary>
+        /// <returns>Высота этажа</returns>
+        public int GetStoreysHeight()
+        {
+            return _height / _storeys;
+        }
+
+        /// <summary>
+        /// Вычисляет кол-во квартир в подъезде
+        /// </summary>
+        /// <returns>кол-во квартир в подъезде</returns>
+        public int GetApartmentsInEntrances()
+        {
+            return _apartments / _entrances;
+        }
+
+        /// <summary>
+        /// Вычисляет кол-во квартир на этаже
+        /// </summary>
+        /// <returns></returns>
+        public int GetApartmentsInStoreys()
+        {
+            return _apartments / _storeys;
+        }
+
+        public void PrintMansion()
+        {
+            PrintId();
+            PrintHeight();
+            PrintStoreys();
+            PrintApartments();
+            PrintEntrances();
+            PrintLine();
+        }
 
         /// <summary>
         /// Создаёт уникальный ID
         /// </summary>
         /// <returns></returns>
-        public int SetId()
+        private int SetId()
         {
-            _mansionNumber = id++;
+            _mansionNumber = ++id;
+            return _mansionNumber;
+        }
+
+        public int GetId()
+        {
             return _mansionNumber;
         }
     }
--- Lesson3.1_Mansion/Lesson3.1_Mansion/Program.cs
+++ Lesson3.1_Mansion/Lesson3.1_Mansion/Program.cs
@@ -10,6 +10,22 @@ namespace Lesson3._1_Mansion
     {
         static void Main(string[] args)
         {
+            Mansion mansion_1 = new Mansion(600, 2, 8, 1);    //Создаём дом высота 6м, 2 этажа, 8 квартир, 1 подъезд
+            Mansion mansion_2 = new Mansion(800, 4, 16, 4);   //Создаём дом высота 6м, 2 этажа, 16 квартир, 2 подъезда
+
+            mansion_1.PrintMansion();
+            mansion_2.PrintMansion();
+
+            Console.WriteLine($"Высота этажей в доме №{mansion_1.GetId()}: {mansion_1.GetStoreysHeight()} см, " +
+                $"Кол-во квартир на этаже: {mansion_1.GetApartmentsInStoreys()}, Кол-во квартир в подъезде {mansion_1.GetApartmentsInEntrances()}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Высота этажей в доме №{mansion_2.GetId()}: {mansion_2.GetStoreysHeight()} см, " +
+                $"Кол-во квартир на этаже: {mansion_2.GetApartmentsInStoreys()}, Кол-во квартир в подъезде {mansion_2.GetApartmentsInEntrances()}");
+
+            Console.ReadLine();
+
         }
     }
 }
