10de2eca2ed5693af4177b133958a92ba3f4e17e
Jackson <dmitrievei92@gmail.com>
2022-05-15 01:38:31 +0300

    final

--- Lesson5.1Calc/Lesson5.1Calc/Program.cs
+++ Lesson5.1Calc/Lesson5.1Calc/Program.cs
@@ -6,12 +6,110 @@ namespace Lesson5._1Calc
     {
         static void Main(string[] args)
         {
-            RationalNumber number1 = new RationalNumber(1, 2);
+            RationalNumber number1 = new RationalNumber(4, 6);
             RationalNumber number2 = new RationalNumber(1, 5);
+            RationalNumber number3 = new RationalNumber(2, 10);
+
+            RationalNumber number = number1 + number2;
+            Console.WriteLine($"{number1.ToString()} + {number2.ToString()} = {number.ToString()}\n");
+
+            number = number1 - number2;
+            Console.WriteLine($"{number1.ToString()} - {number2.ToString()} = {number.ToString()}\n");
+
+            number = number1 * number2;
+            Console.WriteLine($"{number1.ToString()} * {number2.ToString()} = {number.ToString()}\n");
+
+            number = number1 / number2;
+            Console.WriteLine($"{number1.ToString()} / {number2.ToString()} = {number.ToString()}\n");
+
+            number1++;
+            Console.WriteLine($"number1++ = {number1.ToString()} \n");
+
+            number1--;
+            Console.WriteLine($"number1-- = {number1.ToString()} \n");
+
+            number1--;
+            Console.WriteLine($"number1-- = {number1.ToString()} \n");
+
+            bool result = number2 < number3;
+            Console.Write($"{number1.ToString()} < {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+
+
+            result = number2 <= number3;
+            Console.Write($"{number1.ToString()} <= {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+
+
+            result = number1 > number2;
+            Console.Write($"{number1.ToString()} > {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+
+
+            result = number1 >= number2;
+            Console.Write($"{number1.ToString()} >= {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+
+
+            result = number1 == number2;
+            Console.Write($"{number1.ToString()} == {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+
+
+            result = number1 != number2;
+            Console.Write($"{number1.ToString()} != {number2.ToString()} ");
+
+            if (result)
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
 
 
 
-            Console.WriteLine("Hello World!");
         }
     }
 }
--- Lesson5.1Calc/Lesson5.1Calc/RationalNumber.cs
+++ Lesson5.1Calc/Lesson5.1Calc/RationalNumber.cs
@@ -8,15 +8,229 @@ namespace Lesson5._1Calc
 {
     class RationalNumber
     {
-        private double _numerator;
-        private double _denominator;
+        private int _numerator;
+        private int _denominator;
 
-        public RationalNumber(double numerator, double denominator)
+        public RationalNumber(int numerator, int denominator)
         {
+            if (denominator == 0)
+                throw new ArgumentException("Знаменатель не может быть 0");
+
+            if (denominator < 0)
+            {
+                numerator *= -1;
+                denominator *= -1;
+            }
+
             _numerator = numerator;
             _denominator = denominator;
+
+            Minimize();
         }
 
+        /// <summary>
+        /// Находим наибольший общий делитель алгоритмом Евклида
+        /// </summary>
+        /// <param name="number1">1й знаменатель</param>
+        /// <param name="number2">2й знаменатель</param>
+        /// <returns></returns>
+        private static int GetNOD(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            else
+                return GetNOD(number2, number1 % number2);
+        }
 
+        /// <summary>
+        /// Сокращаем дробь до наименьших значений
+        /// </summary>
+        private void Minimize()
+        {
+            int NOD = RationalNumber.GetNOD(_numerator, _denominator);
+            _numerator /= NOD;
+            _denominator /= NOD;
+        }
+
+        /// <summary>
+        /// Преобразует обект в строку
+        /// </summary>
+        /// <returns>строка</returns>
+        public override string ToString()
+        {
+            return $"{_numerator}/{_denominator}";
+        }
+
+        /// <summary>
+        /// Складывает две дроби
+        /// </summary>
+        /// <param name="number1">первая дробь</param>
+        /// <param name="number2">вторая дробь</param>
+        /// <returns>Сумма двух дробей</returns>
+        public static RationalNumber operator +(RationalNumber number1, RationalNumber number2)
+        {
+           return new RationalNumber((number1._numerator * number2._denominator) + (number2._numerator * number1._denominator), number1._denominator * number2._denominator);
+        }
+
+        /// <summary>
+        /// Вычитает из первой дроби вторую
+        /// </summary>
+        /// <param name="number1">первая дробь</param>
+        /// <param name="number2">вторая дробь</param>
+        /// <returns>Разность двух дробей</returns>
+        public static RationalNumber operator -(RationalNumber number1, RationalNumber number2)
+        {
+            return new RationalNumber((number1._numerator * number2._denominator) - (number2._numerator * number1._denominator), number1._denominator * number2._denominator);
+        }
+
+        /// <summary>
+        /// Умножает дроби
+        /// </summary>
+        /// <param name="number1">первая дробь</param>
+        /// <param name="number2">вторая дробь</param>
+        /// <returns>Произведение двух дробей</returns>
+        public static RationalNumber operator *(RationalNumber number1, RationalNumber number2)
+        {
+            return new RationalNumber(number1._numerator * number2._numerator, number1._denominator * number2._denominator);
+        }
+
+        /// <summary>
+        /// Делит первую дробь на вторую
+        /// </summary>
+        /// <param name="number1">первая дробь</param>
+        /// <param name="number2">вторая дробь</param>
+        /// <returns>Частное от двух дробей</returns>
+        public static RationalNumber operator /(RationalNumber number1, RationalNumber number2)
+        {
+            return new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._numerator);
+        }
+
+        //Оператор <
+        public static bool operator <(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator < number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Оператор <=
+        public static bool operator <=(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator <= number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Оператор >=
+        public static bool operator >=(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator >= number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Оператор >
+        public static bool operator >(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator > number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Оператор ==
+        public static bool operator ==(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator == number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Оператор !=
+        public static bool operator !=(RationalNumber number1, RationalNumber number2)
+        {
+            RationalNumber number3 = new RationalNumber(number1._numerator * number2._denominator, number1._denominator * number2._denominator);    //Приводим к общему
+            RationalNumber number4 = new RationalNumber(number2._numerator * number1._denominator, number1._denominator * number2._denominator);    //знаменателю
+
+            //Сравниваем
+            if (number3._numerator != number4._numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Инкремент дроби
+        /// </summary>
+        /// <param name="number">Дробь</param>
+        /// <returns>Результат инкремента</returns>
+        public static RationalNumber operator ++(RationalNumber number)
+        {
+            return new RationalNumber(number._numerator + number._denominator, number._denominator);
+        }
+
+        /// <summary>
+        /// Декремент дроби
+        /// </summary>
+        /// <param name="number">Дробь</param>
+        /// <returns>Результат декремента</returns>
+        public static RationalNumber operator --(RationalNumber number)
+        {
+            return new RationalNumber(number._numerator - number._denominator, number._denominator);
+        }
     }
 }
