2f36af16dd58caa9a03f2420d8aa83d635451ce9
Jackson <dmitrievei92@gmail.com>
2022-05-14 22:51:30 +0300

    update 1

--- Lesson5.1Calc/Lesson5.1Calc/Program.cs
+++ Lesson5.1Calc/Lesson5.1Calc/Program.cs
@@ -6,6 +6,11 @@ namespace Lesson5._1Calc
     {
         static void Main(string[] args)
         {
+            RationalNumber number1 = new RationalNumber(1, 2);
+            RationalNumber number2 = new RationalNumber(1, 5);
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
